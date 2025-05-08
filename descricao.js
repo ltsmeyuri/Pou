10ab6f082399de477763e79874365cba97f79e5b
Yuri <112394310+ltsmeyuri@users.noreply.github.com>
2025-05-08 10:39:37 -0300

    Add files via upload

---
+++ descricao.js
@@ -0,0 +1,8 @@
+function output(t) {
+    document.write('<p>' + t + '</p>');
+  }
+  
+  alert("Welcome to Pou's (sort of) website!! :D");
+  console.log('Sort of an alien, I guess...');
+  output("Take care of him, make him happy, and he'll make you happy too! :)");
+  
